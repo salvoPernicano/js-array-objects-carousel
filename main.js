2c6fcd1c37b89159891dc0d7baaaf2bf2a89222e
salvoPernicano <salvo.pernicano@gmail.com>
2024-01-22 21:33:55 +0100

    feat creazione dinamica item

---
+++ main.js
@@ -0,0 +1,47 @@
+const elementi = [
+    {
+    titolo : "Berserk",
+    desc : "Picco dell'arco narrativo",
+    foto : "./assets/01.jpg"
+    },
+    {
+    titolo : "Fight",
+    desc : "Guts dopo la battaglia",
+    foto : "./assets/02.jpg"
+    },
+    {
+    titolo : "Femto",
+    desc : "Grifis asceso a membro della mano di Dio",
+    foto : "./assets/03.jpg"
+    },
+    {
+    titolo : "Cavaliere del Teschio",
+    desc : "irruzione durante il sacrificio",
+    foto : "./assets/04.jpg"
+    },
+    {
+    titolo : "L'eclissi",
+    desc : "Visioni del passato",
+    foto : "./assets/05.jpg"
+    }
+]
+
+const left = document.getElementById("arrowLeft");
+const right = document.getElementById("arrowRight");
+const railHtml = document.getElementById("railContainer")
+
+left.addEventListener("click", function(){
+    console.log("click left");
+})
+right.addEventListener("click", function(){
+    console.log("click right");
+})
+
+
+for (let i=1; i<=elementi.length; i++){
+    railHtml.innerHTML += `
+    <div class="item">
+    <img src="./assets/0${i}.jpg" alt="">
+    </div>
+    `
+}
